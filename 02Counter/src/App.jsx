6c29cac3e782332b21591d16db2649a70ae7d21b
Vishal <vkant2005@gmail.com>
2025-01-05 12:40:58 +0530

    added notes on why we need hooks

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(0);
+  // useState gives two things- one the variable itself and second an method.
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(++counter);
+    }
+    // counter variable is being increased but there is issue in UI  updation , here comes the role of hooks
+  };
+  const decreaseValue = () => {
+    if (counter > 0) {
+      setCounter(--counter);
+    }
+  };
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>Counter value : {counter}</h2>
+      <button id="add" onClick={addValue}>
+        add Value {counter}
+      </button>
+      <button id="decrease" onClick={decreaseValue}>
+        remove value {counter}
+      </button>
+    </>
+  );
+}
+
+export default App;
