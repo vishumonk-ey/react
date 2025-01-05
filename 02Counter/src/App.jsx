568df055b9a14cf946a0eceb29d694f63eee8eff
Vishal <vkant2005@gmail.com>
2025-01-05 19:00:28 +0530

    added notes on hooks and states

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+// useState is a hook,hooks are used for managing states.
+// states-> can be said as variables but with extra powers
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   let [counter, setCounter] = useState(0);
+  // counter is state here
   // useState gives two things- one the variable itself and second an method.
   const addValue = () => {
     if (counter < 20) {
