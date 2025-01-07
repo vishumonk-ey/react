742d7a62e5acc125e2cd41579ec4c83a6d995c5f
Vishal <vkant2005@gmail.com>
2025-01-07 11:34:30 +0530

    added notes on react snapshot

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -6,12 +6,16 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  let [counter, setCounter] = useState(0);
+  let [counter, setCounter] = useState(1);
   // counter is state here
   // useState gives two things- one the variable itself and second an method.
   const addValue = () => {
     if (counter < 20) {
-      setCounter(++counter);
+      setCounter(++counter); // dont directly increment the state value as counter's local value is updated but react hasnt updated the state yet
+      setCounter(counter*2);
+      setCounter(counter*3);
+//---> react waits for all the state changes to happen and when all the state changes happen it updates the state and re-renders the component
+// react keeps the state variable's value fixed throught the one event handler so i need not worry about whether the value has changed or not.
     }
     // counter variable is being increased but there is issue in UI  updation , here comes the role of hooks
   };
