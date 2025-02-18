0707f2e025b8108fcffd6c037dcc931e0c5ea27d
Vishal <vkant2005@gmail.com>
2025-02-18 12:10:50 +0530

    making pages

---
+++ BlogProject/src/pages/AllPosts.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import appwriteService from  '../appwrite/majorConif'
+import PostCard from '../components/PostCard'
+import Container from '../components/container/Container'
+function AllPosts() {
+    const [posts,setPosts]=React.useState([])
+    appwriteService.getPosts().then((posts) => {
+        setPosts(posts.documents)
+    }).catch((err) => {
+        console.log("error in getting posts",err)
+    })
+  return (
+    <div className='py-8 w-full'>
+        <Container>
+            <div className="flex flex-wrap">
+                {
+                    posts.map((eachPost) => (
+                        <div className="w-full p-2">
+                            <PostCard {...eachPost} />
+                        </div>
+                    ))
+                }
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default AllPosts
