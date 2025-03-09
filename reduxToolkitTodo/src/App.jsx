a33804cd661f6e9534875d25bf2d1849deca3d0d
Vishal <vkant2005@gmail.com>
2025-03-09 18:27:06 +0530

    importing issue resolved

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,29 +1,28 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import TodoForm from './components/addTodo'
-import TodoList from './components/todoList'
-import Navbar from './components/Navbar'
-import { useSelector } from 'react-redux' 
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TodoForm from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import Navbar from "./components/Navbar";
+import { useSelector } from "react-redux";
 function App() {
-  const todos = useSelector((state)=> {
+  const todos = useSelector((state) => {
     // console.log(state.todos);
-    return state.todos
-  }
-)
+    return state.todos;
+  });
   console.log(todos);
-  
-  useEffect(()=>{
-    localStorage.setItem("todos",JSON.stringify(todos))
-  },[todos])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
     <>
-      <Navbar/>
-      <TodoForm/>
-      <TodoList/>
+      <Navbar />
+      <TodoForm />
+      <TodoList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
