b878d5bef5b5b018a7cf345747031b27058752ac
Vishal <vkant2005@gmail.com>
2025-03-21 02:20:51 +0530

    installed libraries

---
+++ blogging/src/assets/config.js
@@ -0,0 +1,5 @@
+const config = {
+    ProjectId : import.meta.env.VITE_APPWRITE_PROJECTID ,
+    DatabaseId : import.meta.env.VITE_APPWRITE_DATABASEID ,
+    CollectionId : import.meta.env.VITE_APPWRITE_COLLECTIONID ,
+}
