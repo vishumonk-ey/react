14ca6903c3217277e1c991cd9d1fb3a9982ae74c
Vishal <vkant2005@gmail.com>
2025-03-23 19:57:03 +0530

    setting up project

--- blogging/src/assets/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    ProjectId : import.meta.env.VITE_APPWRITE_PROJECTID ,
-    DatabaseId : import.meta.env.VITE_APPWRITE_DATABASEID ,
-    CollectionId : import.meta.env.VITE_APPWRITE_COLLECTIONID ,
-}
---
+++ idk/src/assets/config.js
@@ -0,0 +1,5 @@
+export const config = {
+    appwriteProjectId : import.meta.env.VITE_APPWRITE_PROJECTID ,
+    appwriteDatabaseId : import.meta.env.VITE_APPWRITE_DATABASEID ,
+    appwriteCollectionId : import.meta.env.VITE_APPWRITE_COLLECTIONID
+}
