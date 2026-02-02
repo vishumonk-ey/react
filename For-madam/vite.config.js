ead3be13f6e9ae71e3af78e5452755b90c800943
Vishal <vkant2005@gmail.com>
2026-02-02 13:22:59 +0530

    fixing

--- For-madam/vite.config.js
+++ For-madam/vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   css : {
-    transformer : "postcss"
+    transformer : "postcss" ,
   }
 });
